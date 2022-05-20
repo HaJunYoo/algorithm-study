b9850bd8f9b629c8e4899a0d066fb4981a81bf2a
HaJunYoo <hajuny129@gmail.com>
2022-05-20 17:54:56 +0900

    dfs_bfs

---
+++ BFS_DFS/문제/boj_11724.py
@@ -0,0 +1,42 @@
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+# 파이썬의 기본 재귀 한계는 1000인데 10의 6승으로 올린다
+
+input = sys.stdin.readline
+# 빠른 입력 함수
+# 파이썬 내장 input은 조금 느리기 때문에 입력의 최대 호출이 많을 경우 시간 초과가 발생
+
+N, M = map(int, input().split())
+
+adj = [[0] * N for _ in range(N)]
+
+for _ in range(M):
+    # index이기 때문에 1씩 줄여준다
+    a, b = map(lambda x: x - 1, map(int, input().split()))
+    adj[a][b] = adj[b][a] = 1
+
+# 연결요소 개수 변수
+ans = 0
+# 크기가 N개인 배열 생성
+chk = [False] * N
+
+
+# 들어오기 전에 체크를 해주는 방식을 더 추천
+def dfs(now):
+    for nxt in range(N):
+        # now 노드에서 nxt 노드로의 간선이 있고 다음 노드를 방문 한 적이 없다면
+        # 다음 노드 이동 전 chk[nxt]를 True해주고 dfs를 통해 다음 노드 방문
+        if adj[now][nxt] and not chk[nxt]:
+            chk[nxt] = True
+            dfs(nxt)
+
+
+for i in range(N):
+    # 현재 방문한 노드의 chk[i]가 False(not True) 일 경우
+    if not chk[i]:
+        ans += 1
+        chk[i] = True
+        dfs(i)  # dfs를 한번 호출하면 재귀형식으로 연결되어 있는 노드에 한해서 True를 찍어줄 것
+
+print(ans)
---
+++ BFS_DFS/문제/boj_2178.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+N, M = map(int, input().split())
+dy = (0, 1, 0, -1)
+dx = (1, 0, -1, 0)
+
+board = [input() for _ in range(N)]
+
+
+def is_valid_coord(y, x):
+    return 0 <= y < N and 0 <= x <= M
+
+
+def bfs():
+    # check 배열은 격자판과 똑같이 만들어준다
+    chk = [[False] * M for _ in range(N)]
+    chk[0][0] = True
+    dq = deque()
+    # 현재 좌표 y, x 와 간 거리 d
+    dq.append((0, 0, 1))
+    while dq:  # dq 내 원소가 있을 때
+        y, x, d = dq.popleft()
+
+        if y == N - 1 and x == M - 1:
+            return d
+
+        nd = d + 1
+        for k in range(4):
+            ny = y + dy[k]
+            nx = x + dx[k]
+            if is_valid_coord(ny, nx) and board[ny][nx] == '1' and not chk[ny][nx]:
+                chk[ny][nx] = True
+                dq.append((ny, nx, nd))
+
+
+# 만약 목적지에 도착할 수 없는 경우라면 -1을 리턴해라
+# return -1
+
+print(bfs())
