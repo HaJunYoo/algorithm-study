e72c594fa36bbec45a8bfe5512840f514189b040
HaJunYoo <hajuny129@gmail.com>
2022-09-16 09:07:47 +0900

    9/16

---
+++ 백트래킹/BOJ_15649.py
@@ -0,0 +1,33 @@
+n, m = map(int, input().split())
+
+numbers = list()
+
+# 중복되지 않는 순열을 뽑기 위한 방문 확인 리스트
+visited = [False]*(n+1)
+
+
+def print_number():
+    for num in numbers :
+        print(num, end =" ")
+    print()
+
+def choose():
+
+    if len(numbers) == m:
+        print_number()
+        return
+
+    for i in range(1, n+1):
+        if not visited[i] :
+            numbers.append(i)
+            visited[i] = True
+            choose()
+            numbers.pop()
+            visited[i] = False
+
+        # 방문했다면 스킵
+        else :
+            continue
+
+
+choose()
