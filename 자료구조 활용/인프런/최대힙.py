73c7b9bd0c7ac593054968c30425cf99753cb318
hajuny129 <hajuny129@gmail.com>
2022-11-04 17:09:01 +0900

    ✨ feat: 2022 11/4

---
+++ 자료구조 활용/인프런/최대힙.py
@@ -0,0 +1,28 @@
+from heapq import heappush, heappop
+
+heap = list()
+
+# heappush의 시간 복잡 o(log(n))
+# heappop의 시간 복잡도 o(log(n)) -> min heap 자료구조를 유지하는데 시간복잡도 소요
+def decide(x):
+    global flag
+
+    if x == 0:
+        if len(heap) != 0:
+            temp = -1 * heappop(heap) # 루트 노드 pop
+            print(temp)
+        else:
+            print(-1)
+
+    elif x == -1:
+        flag = False
+
+    else :
+        heappush(heap, -1*x)
+
+flag = True
+
+while flag:
+    num = int(input())
+    decide(num)
+
