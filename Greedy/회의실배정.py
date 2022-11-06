47534a03189d0664d632527440f1adc517d8f115
hajuny129 <hajuny129@gmail.com>
2022-11-06 21:31:32 +0900

    ✨ feat: 2022 11/6

---
+++ Greedy/회의실배정.py
@@ -0,0 +1,26 @@
+n = int(input())
+
+s = []
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    s.append((a,b))
+
+s.sort(key = lambda x: (x[1], abs(x[1]-x[0])))
+
+# print(s)
+# [(2, 3), (1, 4), (3, 5), (4, 6), (5, 7)]
+
+start = 0
+end = 0
+cnt = 0
+
+for a, b in s:
+    # print(a, b)
+    if end <= a:
+        start = a
+        end = b
+        # print(start, end)
+        cnt += 1
+
+print(cnt)
