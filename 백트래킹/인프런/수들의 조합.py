3dcc6805626f04379df9db861c7bdf2dc8abeaae
HaJunYoo <hajuny129@gmail.com>
2022-12-01 19:23:32 +0900

    ✨ feat: 2022/12/1

---
+++ 백트래킹/인프런/수들의 조합.py
@@ -0,0 +1,30 @@
+n, k = map(int, input().split())
+nums = list(map(int, input().split()))
+numbers = []
+visited = [False] * (n + 1)
+m = int(input())
+res = 0
+
+
+def combination(level, start):
+    global res
+
+    if level == k:
+        print(numbers, end=' ')
+        if sum(numbers) % m == 0:
+            res += 1
+            print('정답')
+        print()
+        return
+
+    for i in range(start, n):
+        if not visited[i]:
+            visited[i] = True
+            numbers.append(nums[i])
+            combination(level + 1, i + 1)
+            numbers.pop()
+            visited[i] = False
+
+
+combination(0, 0)
+print(res)
--- 백트래킹/인프런/조합 구하기.py
+++ 백트래킹/인프런/조합 구하기.py
@@ -3,15 +3,6 @@ visited = [False] * (n + 1)
 numbers = []
 res = 0
 
-
-def same(i, cnt, start):
-    numbers.append(i)
-    visited[i] = True
-    combination(cnt + 1, start + 1)
-    visited[i] = False
-    numbers.pop()
-
-
 def combination(cnt, start):
     global res
     if cnt == m:
@@ -22,18 +13,15 @@ def combination(cnt, start):
         return
 
     for i in range(start, n + 1):
-        if len(numbers) != 0:
-            if not visited[i] and numbers[-1] < i:
-                same(i, cnt, start)
+        if not visited[i]:
+            numbers.append(i)
+            visited[i] = True
+            combination(cnt + 1, i + 1)
+            visited[i] = False
+            numbers.pop()
 
-            else:
-                continue
         else:
-            if not visited[i]:
-                same(i, cnt, start)
-
-            else:
-                continue
+            continue
 
 
 combination(0, 1)
