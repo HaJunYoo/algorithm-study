3e05e4c766c33efb033b7bba2793c8f8a36fbd7a
HaJunYoo <hajuny129@gmail.com>
2023-03-09 17:40:42 +0900

    ✨ feat: 2023/3/9

---
+++ 완전탐색/Programmers/모음 사전.py
@@ -0,0 +1,24 @@
+## 풀이 1
+
+from itertools import product
+
+def solution(word):
+    words = []
+    for i in range(1, 6):
+        for c in product(['A', 'E', 'I', 'O', 'U'], repeat=i):
+            words.append(''.join(list(c)))
+
+    words.sort()
+    return words.index(word) + 1
+
+
+### 풀이 2
+
+def solution(word):
+    words = []
+    for i in range(1, 6):
+        for c in product(['A', 'E', 'I', 'O', 'U'], repeat=i):
+            words.append(''.join(list(c)))
+
+    words.sort()
+    return words.index(word) + 1
