95a5941fc4c06cc2b726208766ca11760a29661c
HaJunYoo <hajuny129@gmail.com>
2023-07-13 22:23:35 +0900

    ✨ feat: 2023/7/13

---
+++ DP/BOJ/BOJ_2225(합분해).py
@@ -0,0 +1,18 @@
+
+
+if __name__ == '__main__':
+    n, k = map(int,input().split())
+    
+    # [k, n]
+    dp = [[0]*201 for _ in range(201)]
+    
+    for i in range(201):
+        dp[1][i] = 1
+        dp[2][i] = i+1
+    
+    for i in range(2, 201):
+        dp[i][1] = i
+        for j in range(2, 201):
+            dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % 1000000000
+    
+    print(dp[k][n])
