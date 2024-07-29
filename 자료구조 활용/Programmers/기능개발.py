982c179aa1d88afcb7a1aacec867a2cc326eb77d
유하준 <hajuny129@gmail.com>
2024-07-30 00:25:52 +0900

    240730

--- 자료구조 활용/Programmers/기능개발.py
+++ 자료구조 활용/Programmers/기능개발.py
@@ -1,32 +1,41 @@
-import math
 from collections import deque
+import math
 
+
+def ceil(num):
+    if num - int(num) > 0:
+        return int(num + 1)
+    else:
+        return int(num)
 
 def solution(progresses, speeds):
-    answer = []
     days = deque()
     for progress, speed in zip(progresses, speeds):
         day = (100 - progress) / speed
-        day = math.ceil(day)
-        days.append(day)
+
+        days.append(ceil(num=day))
+
     print(days)
-
+    answer = []
+    f = 0
     while days:
-        for i in range(len(days)):
-            if days[i] == 0:
-                continue
-            days[i] -= 1
-
-        if days and days[0] == 0:
-            cnt = 0
-            flag = True
-            while flag and days:
-                if days[0] == 0:
+        cnt = 0
+        while True:
+            if days:
+                if days[0] <= f:
                     days.popleft()
                     cnt += 1
                 else:
-                    flag = False
+                    break
+            else:
+                break
+        if cnt > 0:
             answer.append(cnt)
+        f += 1
 
-    print(answer)
     return answer
+
+progresses = [95, 90, 99, 99, 80, 99]
+speeds = [1, 1, 1, 1, 1, 1]
+print(solution(progresses, speeds))
+
