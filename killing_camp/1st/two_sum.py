725650c0a945fbcd819a956420a7d7889d3f0bf1
HaJunYoo <hajuny129@gmail.com>
2025-06-02 16:52:31 +0900

    250602 twosum (2)

--- killing_camp/1st/two_sum.py
+++ killing_camp/1st/two_sum.py
@@ -2,11 +2,20 @@ nums = [4, 9, 7, 5, 1]
 target = 17
 ans = []
 
+def twosum():
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            for k in range(j+1, len(nums)):
+                if nums[i] + nums[j] + nums[k] == target:
+                    print(f'two sum : {i}, {j}, {k}')
+                    return [i, j, k]
+
+
 def backtracking(start):
     if len(ans) == 3:
         # print(nums[ans[0]], nums[ans[1]])
         if nums[ans[0]] + nums[ans[1]] + nums[ans[2]] == target:
-            print(ans)
+            print(f'back tracking : {ans}')
             return ans
         return False
 
@@ -16,4 +25,5 @@ def backtracking(start):
             return ans
         ans.pop()
 
+twosum()
 backtracking(0)
