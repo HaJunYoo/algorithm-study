c2c9f55effb9133168c1c6a3db68230719157feb
유하준 <hajuny129@gmail.com>
2025-10-09 01:40:55 +0900

    add: 251008 word search

--- brute_force/python/leetcode/word_search.py
+++ brute_force/python/leetcode/word_search.py
@@ -5,43 +5,35 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         len_x, len_y = len(board), len(board[0])
         visited = [[False for _ in range(len_y)] for _ in range(len_x)]
-        print(visited)
         dxs, dys = [0, 0, 1, -1], [1, -1, 0, 0]
 
-        def valid_in_range(x, y):
-            if 0 <= x < len_x and 0 <= y < len_y:
-                return True
-            else:
-                return False
-
-        def search(x: int, y: int, path: int, visited: List[List[bool]]):
-
-            if not visited[x][y] and board[x][y] == word[path]:
-                if path == len(word) - 1:
-                    # print(path)
+        def search(x, y, w_index, visited):
+            word_char = word[w_index]
+            if not visited[x][y] and board[x][y] == word_char:
+                if w_index == len(word) - 1:
                     return True
-
+            
                 visited[x][y] = True
                 flag = False
 
                 for dx, dy in zip(dxs, dys):
                     nx, ny = x + dx, y + dy
-                    if valid_in_range(nx, ny):
-                        if search(nx, ny, path+1, visited):
-                            flag = True
-                            return flag
+                    if 0 <= nx < len_x and 0 <= ny < len_y:
+                        flag = search(nx, ny, w_index+1, visited)
+                        if flag:
+                            return True
 
                 visited[x][y] = False
                 return flag
 
-        for x in range(len_x):
-            for y in range(len_y):
-                # print(f'{x}, {y}')
-                if search(x, y, 0, visited):
+            return False
+
+        for i in range(len_x):
+            for j in range(len_y):
+                if search(i, j, 0, visited):
                     return True
-
+        
         return False
-
 
 if __name__ == '__main__':
     solution = Solution()
