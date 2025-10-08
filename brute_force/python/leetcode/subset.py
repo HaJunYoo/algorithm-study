4a5a0f3d33a4835d8b1afc0ec99e053cd39be905
유하준 <hajuny129@gmail.com>
2025-10-09 02:19:26 +0900

    add: 251008 subset

--- brute_force/python/leetcode/subset.py
+++ brute_force/python/leetcode/subset.py
@@ -3,18 +3,27 @@ from typing import List
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def dfs(start: int, stack: List[int]):
+        result = []
+
+        def dfs(start, stack):
             print(stack)
+            # 중복 조건이긴 하나 안전하게 처리하기 위해서 추가
+            if len(stack) == len(nums):
+                return
+            
             result.append(stack[:])
 
             for i in range(start, len(nums)):
                 stack.append(nums[i])
-                dfs(i + 1, stack)
+                dfs(i+1, stack)
                 stack.pop()
-
+        
         start = 0
-        result = []
-        stack = []
+        stack = [] # 트리의 깊이
         dfs(start, stack)
 
         return result
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.subsets([1, 2, 3]))
