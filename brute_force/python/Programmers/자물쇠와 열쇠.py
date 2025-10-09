168bcfe7e90ca54af3b7d164de5926dfb459186f
유하준 <hajuny129@gmail.com>
2025-10-10 02:50:39 +0900

    add: 251009 자물쇠와 열쇠

--- brute_force/python/Programmers/자물쇠와 열쇠.py
+++ brute_force/python/Programmers/자물쇠와 열쇠.py
@@ -1,64 +1,30 @@
 from typing import List
 
-
-def attach(x, y, M, key, board):
-    # 현재 보드판 위치 기준으로 키를 위치시킨다
-    for i in range(M):
-        for j in range(M):
-            board[x + i][y + j] += key[i][j]
-
-
-def detach(x, y, M, key, board):
-    # 위치 시킨 키를 다시 제거한다
-    for i in range(M):
-        for j in range(M):
-            board[x + i][y + j] -= key[i][j]
-
-
-def rotate_keys(key: List, M):
-    ret = [[0] * M for _ in range(M)]
-    # 4번
-    for r in range(M):
-        for c in range(M):
-            ret[c][M - 1 - r] = key[r][c]
-    # ret = key[:]
-    # ret = list(zip(*ret[::-1]))
-    return ret
-
-
-def check_fit(board, M, N):
-    # lock 위치에 하나라도 비어 있다면 그것은 안 맞는 것
-    for i in range(N):
-        for j in range(N):
-            if board[M + i][M + j] != 1:
-                return False
-    return True
-
-
 def solution(key, lock):
-    M, N = len(key), len(lock)
+    m = len(key)
+    n = len(lock)
+    keys = [[[0 for _ in range(m+2*n)] for _ in range(m+2*n)] for _ in range(4)]
 
-    board = [[0] * (M * 2 + N) for _ in range(M * 2 + N)]
-
-    # print(board)
-    for i in range(N):
-        for j in range(N):
-            # key 기준으로 i, j을 배치 (가운데 배치)
-            board[M + i][M + j] = lock[i][j]
-
-    # print(board)
-    rotated_key = key
-    # 모든 방향 (4번 루프)
-    for _ in range(4):
-        rotated_key = rotate_keys(rotated_key, M)
-        # 겹치기 위해서는 1부터 시작해야한다
-        for x in range(1, M + N):
-            for y in range(1, M + N):
-                attach(x, y, M, rotated_key, board)
-                if (check_fit(board, M, N)):
+    if all(lock[i][j] for i in range(n) for j in range(n)):
+        return True
+    
+    for i in range(m):
+        for j in range(m):
+            keys[0][n+i][n+j] = key[i][j]
+            keys[1][n+j][m+n-1-i] = key[i][j]
+            keys[2][m+n-1-i][m+n-1-j] = key[i][j]
+            keys[3][m+n-1-j][n+i] = key[i][j]
+    
+    for k in keys:
+        for i in range(m+n):
+            for j in range(m+n):
+                flag = True
+                for ii in range(n):
+                    for jj in range(n):
+                        if not lock[ii][jj] ^ k[i+ii][j+jj]:
+                            flag = False
+                if flag:
                     return True
-                detach(x, y, M, rotated_key, board)
-
     return False
 
 
